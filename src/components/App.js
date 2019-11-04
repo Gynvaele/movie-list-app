3c7af1ee1aaa1a983f0a7cb35d533cc1f57b577f
Pasha Klimenko <88pasha88@gmail.com>
2019-11-04 21:01:53 +0200

    intellij-changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,96 +1,96 @@
-import React, { PureComponent } from "react";
-import { moviesData } from "../moviesData";
+import React, {PureComponent} from "react";
+import {moviesData} from "../moviesData";
 import MovieItem from "./MovieItem";
 import "./index.scss";
 
 // UI = fn(state, props)
 
 class App extends PureComponent {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      movies: moviesData,
-      moviesWillWatch: [],
+        this.state = {
+            movies: moviesData,
+            moviesWillWatch: [],
+        };
+
+        // this.removeMovie = this.removeMovie.bind(this);
+    }
+
+    removeMovie = movie => {
+        const updateMovies = this.state.movies.filter(function (item) {
+            return item.id !== movie.id;
+        });
+        this.setState({
+            movies: updateMovies,
+        });
     };
 
-    // this.removeMovie = this.removeMovie.bind(this);
-  }
+    addMovieToWillWatch = movie => {
+        // const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+        // updateMoviesWillWatch.push(movie);
 
-  removeMovie = movie => {
-    const updateMovies = this.state.movies.filter(function(item) {
-      return item.id !== movie.id;
-    });
-    this.setState({
-      movies: updateMovies,
-    });
-  };
+        const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
 
-  addMovieToWillWatch = movie => {
-    // const updateMoviesWillWatch = [...this.state.moviesWillWatch];
-    // updateMoviesWillWatch.push(movie);
+        this.setState({
+            moviesWillWatch: updateMoviesWillWatch,
+        });
+    };
 
-    const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+    removeMovieFromWillWatch = movie => {
+        const updateMoviesFromWillWatch = this.state.moviesWillWatch.filter(function (item) {
+            return item.id !== movie.id;
+        });
+        this.setState({
+            moviesWillWatch: updateMoviesFromWillWatch,
+        });
+    };
 
-    this.setState({
-      moviesWillWatch: updateMoviesWillWatch,
-    });
-  };
-
-  removeMovieFromWillWatch = movie => {
-    const updateMoviesFromWillWatch = this.state.moviesWillWatch.filter(function(item) {
-      return item.id !== movie.id;
-    });
-    this.setState({
-      moviesWillWatch: updateMoviesFromWillWatch,
-    });
-  };
-
-  render() {
-    console.log(this.state.moviesWillWatch);
-    return (
-      <div className="container  pt-3">
-        <div className="row">
-          <div className="md col-9 ">
-            <div className="row">
-              {this.state.movies.map(movie => {
-                return (
-                  <div className="col-6 mb-4" key={movie.id}>
-                    <MovieItem
-                      movie={movie}
-                      removeMovie={this.removeMovie}
-                      addMovieToWillWatch={this.addMovieToWillWatch}
-                      removeMovieFromWillWatch={this.removeMovieFromWillWatch}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="col-3">
-            <p>Will Watch: {this.state.moviesWillWatch.length}</p>
-            <div>
-              {this.state.moviesWillWatch.length === 0 ? (
-                ""
-              ) : (
-                <div className="card p-1">
-                  {this.state.moviesWillWatch.map(willList => {
-                    return (
-                      <div key={willList.id}>
-                        <div className="movieList">
-                          {willList.title} Rate: {willList.vote_average}
+    render() {
+        console.log(this.state.moviesWillWatch);
+        return (
+            <div className="container  pt-3">
+                <div className="row">
+                    <div className="md col-9 ">
+                        <div className="row">
+                            {this.state.movies.map(movie => {
+                                return (
+                                    <div className="col-6 mb-4" key={movie.id}>
+                                        <MovieItem
+                                            movie={movie}
+                                            removeMovie={this.removeMovie}
+                                            addMovieToWillWatch={this.addMovieToWillWatch}
+                                            removeMovieFromWillWatch={this.removeMovieFromWillWatch}
+                                        />
+                                    </div>
+                                );
+                            })}
                         </div>
-                      </div>
-                    );
-                  })}
+                    </div>
+                    <div className="col-3">
+                        <p>Will Watch: {this.state.moviesWillWatch.length}</p>
+                        <div>
+                            {this.state.moviesWillWatch.length === 0 ? (
+                                ""
+                            ) : (
+                                <div className="card p-1">
+                                    {this.state.moviesWillWatch.map(willList => {
+                                        return (
+                                            <div key={willList.id}>
+                                                <div className="movieList">
+                                                    {willList.title} Rate: {willList.vote_average}
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
-              )}
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,75 +1,75 @@
-import React, { PureComponent } from "react";
+import React, {PureComponent} from "react";
 
 class MovieItem extends PureComponent {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      addWatch: false,
-    };
-  }
+        this.state = {
+            addWatch: false,
+        };
+    }
 
-  render() {
-    const { movie, removeMovie, addMovieToWillWatch, removeMovieFromWillWatch } = this.props;
-    return (
-      <div className="card p-1">
-        <img
-          className="card-img-top"
-          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
-          alt=""
-        />
-        <div className="card-body">
-          <div className="d-flex justify-content-between align-items-center">
-            <h6 className="card-title">{movie.title}</h6>
-            <h6 className="text-secondary">Rating: {movie.vote_average}</h6>
-          </div>
-          <div className="d-flex justify-content-between align-items-center mb-2">
-            {this.state.addWatch === true ? (
-              <button
-                type="button"
-                className="btn btn-success"
-                style={{ fontSize: "0.8rem" }}
-                onClick={() => {
-                  this.setState({
-                    addWatch: false,
-                  });
-                  removeMovieFromWillWatch(movie);
-                }}
-              >
-                Remove movie
-              </button>
-            ) : (
-              <button
-                type="button"
-                className="btn btn-secondary"
-                style={{ fontSize: "0.8rem" }}
-                onClick={() => {
-                  this.setState({
-                    addWatch: true,
-                  });
-                  addMovieToWillWatch(movie);
-                }}
-              >
-                Will Watch
-              </button>
-            )}
-            <button
-              type="button"
-              className="btn btn-dark"
-              onClick={removeMovie.bind(null, movie)}
-              style={{ fontSize: "0.8rem" }}
-            >
-              Delete movie
-            </button>
-          </div>
-          <div className="overflow-auto" style={{ height: "200px", fontSize: ".8rem" }}>
-            <h6>Overview:</h6>
-            {movie.overview}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const {movie, removeMovie, addMovieToWillWatch, removeMovieFromWillWatch} = this.props;
+        return (
+            <div className="card p-1">
+                <img
+                    className="card-img-top"
+                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
+                    alt=""
+                />
+                <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h6 className="card-title">{movie.title}</h6>
+                        <h6 className="text-secondary">Rating: {movie.vote_average}</h6>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center mb-2">
+                        {this.state.addWatch === true ? (
+                            <button
+                                type="button"
+                                className="btn btn-success"
+                                style={{fontSize: "0.8rem"}}
+                                onClick={() => {
+                                    this.setState({
+                                        addWatch: false,
+                                    });
+                                    removeMovieFromWillWatch(movie);
+                                }}
+                            >
+                                Remove movie
+                            </button>
+                        ) : (
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                style={{fontSize: "0.8rem"}}
+                                onClick={() => {
+                                    this.setState({
+                                        addWatch: true,
+                                    });
+                                    addMovieToWillWatch(movie);
+                                }}
+                            >
+                                Will Watch
+                            </button>
+                        )}
+                        <button
+                            type="button"
+                            className="btn btn-dark"
+                            onClick={removeMovie.bind(null, movie)}
+                            style={{fontSize: "0.8rem"}}
+                        >
+                            Delete movie
+                        </button>
+                    </div>
+                    <div className="overflow-auto" style={{height: "200px", fontSize: ".8rem"}}>
+                        <h6>Overview:</h6>
+                        {movie.overview}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default MovieItem;
